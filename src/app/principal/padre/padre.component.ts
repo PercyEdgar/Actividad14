2fbbd2ebaf6f51173e155714633090b1ae4936cb
PercyEdgar <1percyqq@gmail.com>
2023-01-15 10:46:37 -0500

    Reto Actividad 14 culminado

--- src/app/principal/padre/padre.component.ts
+++ src/app/principal/padre/padre.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./padre.component.css']
 })
 export class PadreComponent {
+datos_padre: string = "";
 
 nombre: string ="";
 dni: string ="";
